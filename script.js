fca207d6f934855958df5ed37ee94047d873af3f
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-03-14 14:30:53 +0200

    completed syntax validation

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function validateSyntax() {
-  let input = document.getElementById('petInput').value;
+  let input = document.getElementById("petInput").value;
   // Validation logic goes here
-  let result = ''; // Placeholder for validation result
+  let result = ""; // Placeholder for validation result
 
-  // TODO: Write your validation logic here
-      // Check if input starts with 'pet_' and followed by alphanumeric characters
+  const characters = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
 
-          document.getElementById('result').innerText = result;
+  if (input.startsWith("pet_") && characters.test(input.substring(4))){
+    result = "Valid Syntax";
+    console.log("Valid Syntax");
+  } 
+  else {
+    result = "Invalid Syntax";
+    console.log("Invalid Syntax")
+  }
+
+  document.getElementById("result").innerText = result;
 }
