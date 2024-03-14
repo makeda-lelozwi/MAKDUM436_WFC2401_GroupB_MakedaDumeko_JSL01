d5d69100b4beb6a8dc34486504822724b07a72b7
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-03-14 14:40:11 +0200

    added the dots

--- script.js
+++ script.js
@@ -4,14 +4,19 @@ function validateSyntax() {
   let result = ""; // Placeholder for validation result
 
   const characters = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
+  //The pet's name must start with a capital letter 
+
+  const dot = document.getElementById("dot")
 
   if (input.startsWith("pet_") && characters.test(input.substring(4))){
     result = "Valid Syntax";
     console.log("Valid Syntax");
+    dot.style.backgroundColor = "green";
   } 
   else {
     result = "Invalid Syntax";
-    console.log("Invalid Syntax")
+    console.log("Invalid Syntax");
+    dot.style.backgroundColor = "red";
   }
 
   document.getElementById("result").innerText = result;
