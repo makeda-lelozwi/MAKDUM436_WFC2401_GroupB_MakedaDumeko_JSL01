bdccbbc1c0645a4319507769f017d99a5c8ab5f4
Makeda Lelozwi Dumeko <lelodumeko2@gmail.com>
2024-03-14 22:37:38 +0200

    final changes

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ function validateSyntax() {
   // Validation logic goes here
   let result = ""; // Placeholder for validation result
 
-  const characters = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/
+  const characters = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
   //The pet's name must start with a capital letter 
 
-  const dot = document.getElementById("dot")
-
+  const dot = document.getElementById("dot");
+  
   if (input.startsWith("pet_") && characters.test(input.substring(4))){
+    /*JavaScript strings start counting from 0. The "pet_" part takes up spaces 0 - 3.
+    So that's why we want our characters to start from 4 (substring(4)). 
+
+    */
     result = "Valid Syntax";
     console.log("Valid Syntax");
     dot.style.backgroundColor = "green";
